f83cbcf5decc29555a51c9aa3b2bdac8ce581dda
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2023-11-18 18:46:15 +0530

    login and signup with rtk

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { useDispatch } from 'react-redux';
+import { login } from '../Users/userSlice';
+import { Link } from 'react-router-dom';
 
 function Login() {
   const {register, handleSubmit}= useForm();
+  const dispatch = useDispatch();
+
   return (
     <div>
-        <form onSubmit={handleSubmit((data)=>console.log(data))}>
+        <form onSubmit={handleSubmit((data)=>{
+            dispatch(login(data))
+        })}>
      
       <label>College email:</label>
       <input type="email" id='email' { ...register("email")} />
@@ -13,7 +20,8 @@ function Login() {
       <label>Password:</label>
       <input type="password" id='password' { ...register("password")} />
       <br/>
-    
+        
+      <Link>Forgot password?</Link>
       <button type='submit'>log in</button>
      </form>
     </div>
