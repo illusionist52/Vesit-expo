0f259ff42ec64ca058781e83c952f5e344884523
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2023-12-06 19:02:36 +0530

    Log out button added in hamburger menu

--- client/src/Components/HamNav.jsx
+++ client/src/Components/HamNav.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import Button from "./Button";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "../Users/userSlice";
+import toast from "react-hot-toast";
 
 function HamNav({ toggleHamburger, setToggleHamburger }) {
   const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   if (toggleHamburger)
     return (
-      <div className={`w-full absolute z-50 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20 border border-gray-100 text-slate-200 transition-all duration-1000  ${toggleHamburger ? "-translate-y-" : "-translate-y-full"}`}>
+      <div className={`w-full absolute z-50 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20 border border-gray-100 text-slate-200 transition-all duration-1000  ${toggleHamburger ? "-translate-y-" : "-translate-y-full"} md:hidden`}>
         <ul className="flex flex-col items-center my-5 gap-5 h-[100%] w-[100%]">
           <li className="">
             <Button
@@ -47,7 +52,24 @@ function HamNav({ toggleHamburger, setToggleHamburger }) {
             >
               Companies
             </Button>
+          
           </li>
+          {user.name &&   
+          <li className=" ">
+          <Button
+              style={"custom"}
+              className={"border-none text-xl font-bold py-4 px-[100vh]"}
+              active={"lavender"}
+              onClick={() => {
+                dispatch(logout());
+                setToggleHamburger(false)
+                toast.success("logged out")
+              }}
+            >
+              Logout
+            </Button>
+          
+          </li>}
         </ul>
       </div>
     );
